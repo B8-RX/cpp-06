68911f68c96d9cdae8e3692e95adc78a2548f49c
Sofiane Sghiouar <sofiane.sghiouar@gmail.com>
2025-12-02 07:18:32 +0100

    cpp06/ex01: first push

---
+++ ex01/Serializer.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssghioua <ssghioua@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 07:11:30 by ssghioua          #+#    #+#             */
+/*   Updated: 2025/12/02 07:11:31 by ssghioua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+
+#include <string>
+
+class Serializer {
+	public:
+		static unintptr_t	serialize(Data* ptr);
+		static Data*		deserialize(unintptr_t raw);
+	private:
+		Serializer(void);
+		~Serializer(void);
+		Serializer(const Serializer&);
+		Serializer&	operator=(const Serializer&);
+};
+
+#endif // !SERIALIZER_HPP
